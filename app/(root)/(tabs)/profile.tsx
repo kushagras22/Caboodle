0c0c8ceef53d73907855fb8c6cdc2b05ecbadbd3
Kushagra Sahu <kushagra.sawho22@gmail.com>
2025-06-26 19:28:52 +0530

    Authentication Integration

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function Profile() {
+  return (
+    <SafeAreaView className=" flex-1 items-center justify-center">
+      <Text className="text-4xl font-semibold text-red-500">
+        Welcome to Profile!
+      </Text>
+    </SafeAreaView>
+  );
+}
